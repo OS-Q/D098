091c40198e44c7a62f235db733ed51426c0fd221
qitas <qitas@qitas.cn>
2019-08-20 10:26:13 +0800

    fix add

---
+++ src/NTPtime.h
@@ -0,0 +1,41 @@
+#ifndef NTPtime_h
+#define NTPtime_h
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+struct strDateTime
+{
+  byte hour;
+  byte minute;
+  byte second;
+  int year;
+  byte month;
+  byte day;
+  byte dayofWeek;
+  unsigned long epochTime;
+  boolean valid;
+};
+
+class NTPtime {
+  public:
+    NTPtime(String NTPtime);
+    strDateTime getNTPtime(float _timeZone, int _DayLightSaving);
+    void printDateTime(strDateTime _dateTime);
+    bool setSendInterval(unsigned long _sendInterval);  // in seconds
+    bool setRecvTimeout(unsigned long _recvTimeout);    // in seconds
+
+  private:
+    bool _sendPhase;
+    unsigned long _sentTime;
+    unsigned long _sendInterval;
+    unsigned long _recvTimeout;
+
+    strDateTime ConvertUnixTimestamp( unsigned long _tempTimeStamp);
+    boolean summerTime(unsigned long _timeStamp );
+    boolean daylightSavingTime(unsigned long _timeStamp);
+    unsigned long adjustTimeZone(unsigned long _timeStamp, float _timeZone, int _DayLightSavingSaving);
+    WiFiUDP UDPNTPClient;
+};
+#endif
