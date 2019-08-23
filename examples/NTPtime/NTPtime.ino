945e1f3db5d769b32e385b51c50f4705f7eaf9b3
qitas <qitas@qitas.cn>
2019-08-23 09:43:30 +0800

    fix add more

--- examples/NTPtime/NTPtime.ino
+++ examples/NTPtime/NTPtime.ino
@@ -38,12 +38,10 @@ void loop() {
 
   // first parameter: Time zone in floating point; second parameter: 1 for European summer time; 2 for US daylight saving time; 0 for no DST adjustment; 
   dateTime = NTPch.getNTPtime(8.0, 0);
-
   // check dateTime.valid before using the returned time
   // Use "setSendInterval" or "setRecvTimeout" if required
   if(dateTime.valid){
     NTPch.printDateTime(dateTime);
-
     byte actualHour = dateTime.hour;
     byte actualMinute = dateTime.minute;
     byte actualsecond = dateTime.second;
